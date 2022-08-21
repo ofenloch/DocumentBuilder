7f9d7653fcbd010633147a0d2b659d6355c72062
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2022-08-21 06:45:38 +0000

    add constructor to class dblib.DocumentBuilder

--- lib/DocumentBuilder.cs
+++ lib/DocumentBuilder.cs
@@ -1,5 +1,19 @@
-﻿namespace dblib;
-public class DocumentBuilder
+﻿namespace dblib
 {
+    public class DocumentBuilder
+    {
 
-}
+        private string _outputDirectory = "";
+        private string _outputFileName = "";
+        private string _outputFileBaseName = "";
+        public DocumentBuilder(string templateFileName, string xmlDataFileName, string outputFileName)
+        {
+            _outputFileName = Path.GetFullPath(outputFileName);
+            _outputFileBaseName = Path.GetFileNameWithoutExtension(_outputFileName);
+            _outputDirectory = Path.GetDirectoryName(_outputFileName);
+            Utilities.CreateDirectory(_outputDirectory);
+        }
+
+    } // public class DocumentBuilder
+
+} // namespace dblib
