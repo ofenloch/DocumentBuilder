935bdf795162be5f7b51f857b0540a1b59cea219
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2022-08-21 06:46:11 +0000

    add some code using dblib

--- DocumentBuilder/Program.cs
+++ DocumentBuilder/Program.cs
@@ -2,6 +2,7 @@
 // See https://aka.ms/new-console-template for more information
 
 using System;
+
 namespace DocumentBuilder
 {
     internal class Program
@@ -9,6 +10,24 @@ namespace DocumentBuilder
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string outDir = dblib.Utilities.CreateDirectory("./output/");
+            Console.WriteLine("output directory is {0}", outDir);
+            
+            string dataDir = dblib.Utilities.CreateDirectory("./data/");
+            Console.WriteLine("data directory is {0}", dataDir);
+
+            string templateWord = Path.Combine(dataDir, "template.docx");
+            Console.WriteLine("Word template is {0}", templateWord);
+            string templateExcel = Path.Combine(dataDir, "template.xlsx");
+            Console.WriteLine("Excel template is {0}", templateExcel);
+
+            string xmlDataFileName = Path.Combine(dataDir, "template.xml");
+
+
+            dblib.DocumentBuilder dbExcel = new dblib.DocumentBuilder(templateExcel, xmlDataFileName, Path.Combine(outDir, "example.xlsx"));
+            dblib.DocumentBuilder dbWord = new dblib.DocumentBuilder(templateWord, xmlDataFileName, Path.Combine(outDir, "example.docx"));
+
         }
     } // internal class Program
 } // namespace DocumentBuilder
