52bbba600ef41c986de67f4c10bc4676b0d8d0f1
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2022-08-22 21:16:31 +0000

    actually do some testing

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,12 +1,31 @@
+using System.IO;
 using Xunit;
 
 namespace unit_tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+    static string _outDir = Path.GetFullPath("./output");
 
+    [Fact]
+    public void TestMethod()
+    {
+        Assert.False(true, "Expected path " + Directory.GetCurrentDirectory());
+    }
+
+    [Fact]
+    public void test_DocumentBuilder_Basic()
+    {
+        string testDocx = Path.Combine(_outDir, "test.docx");
+        Logger.Debug("test_DocumentBuilder_Basic: testDocx is {0}", testDocx);
+        string testXlsx = Path.Combine(_outDir, "test.xlsx");
+        Logger.Debug("test_DocumentBuilder_Basic: testXlsx is {0}", testXlsx);
+
+        dblib.DocumentBuilder.CreateNewWordDocument(testDocx);
+        Assert.True(File.Exists(testDocx), $"file {testDocx} should exist");
+
+        dblib.DocumentBuilder.CreateNewExcelDocument(testXlsx);
+        Assert.True(File.Exists(testXlsx), $"file {testXlsx} should exist");
     }
 }
